745c7f21a72145033429f0844ede21507386e3e8
Lucas DUTRIEZ <ldutriez@e1r11p2.clusters.42paris.fr>
2023-01-01 15:28:35 +0100

    ✅ Add tests. One about non whole exponents. Two were every real numbers are solution to the equation.

--- tests/unit_test.sh
+++ tests/unit_test.sh
@@ -13,12 +13,15 @@ fi
 ./"$FILE" "52 - 20 + 10 = 42" >> "$OUTPUT_FILE" 2>&1
 ./"$FILE" "42" >> "$OUTPUT_FILE" 2>&1
 ./"$FILE" "42^0 = 1" >> "$OUTPUT_FILE" 2>&1
+./"$FILE" "10^1.5 = 31.6227766" >> "$OUTPUT_FILE" 2>&1
 ./"$FILE" "5^" >> "$OUTPUT_FILE" 2>&1
 ./"$FILE" "5^six" >> "$OUTPUT_FILE" 2>&1
 ./"$FILE" "quatre egal 4" >> "$OUTPUT_FILE" 2>&1
 ./"$FILE" "6.480740698^2 = 42" >> "$OUTPUT_FILE" 2>&1
 ./"$FILE" "54e2 = 0" >> "$OUTPUT_FILE" 2>&1
 ./"$FILE" "42.000000000000000000000000000000000000002 = 42" >> "$OUTPUT_FILE" 2>&1
+./"$FILE" "42 * X^0 = 42 * X^0" >> "$OUTPUT_FILE" 2>&1
+./"$FILE" "42 * X^1 = 42 * X^1" >> "$OUTPUT_FILE" 2>&1
 ./"$FILE" "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0" >> "$OUTPUT_FILE" 2>&1
 ./"$FILE" "8 * X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = 3 * X^0" >> "$OUTPUT_FILE" 2>&1
 
