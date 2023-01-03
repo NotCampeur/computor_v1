a12eeff4786290529b7f47d14d5e2cd0f003d125
Lucas DUTRIEZ <ldutriez@e1r11p3.clusters.42paris.fr>
2023-01-03 05:20:51 +0100

    🏗️ Split FormulaParser::parse_formula into submethodes.

--- srcs/Equation/FormulaParser.hpp
+++ srcs/Equation/FormulaParser.hpp
@@ -6,7 +6,7 @@
 /*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 04:42:43 by ldutriez          #+#    #+#             */
-/*   Updated: 2023/01/03 04:49:37 by ldutriez         ###   ########.fr       */
+/*   Updated: 2023/01/03 05:19:16 by ldutriez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,20 @@ class FormulaParser
 {
 	public:
 		FormulaParser(const std::string &formula)
-		: _first_expression_terms(), _second_expression_terms()
+		: _first_expression_terms(), _second_expression_terms(), _i(0)
+		, _spaceless_formula(formula), _current_expression_terms(&_first_expression_terms)
+		, _current_coefficient(0.0f), _current_unknown_degree(0)
+		, _is_constant(true), _is_negative(false), _is_exponant(false), _is_multiplication(false)
 		{
-			std::string spaceless_formula(formula);
-			for (auto it(spaceless_formula.begin()); it != spaceless_formula.end(); ++it)
+			for (auto it(_spaceless_formula.begin()); it != _spaceless_formula.end(); ++it)
 			{
 				if (*it == ' ')
 				{
-					spaceless_formula.erase(it);
+					_spaceless_formula.erase(it);
 					it--;
 				}
 			}
-			parse_formula(spaceless_formula);
+			parse_formula();
 		}
 		
 		~FormulaParser()
@@ -43,6 +45,12 @@ class FormulaParser
 		FormulaParser(FormulaParser const &obj)
 		: _first_expression_terms(obj._first_expression_terms)
 		, _second_expression_terms(obj._second_expression_terms)
+		, _i(obj._i), _spaceless_formula(obj._spaceless_formula)
+		, _current_expression_terms(obj._current_expression_terms)
+		, _current_coefficient(obj._current_coefficient)
+		, _current_unknown_degree(obj._current_unknown_degree)
+		, _is_constant(obj._is_constant), _is_negative(obj._is_negative)
+		, _is_exponant(obj._is_exponant), _is_multiplication(obj._is_multiplication)
 		{}
 
 		FormulaParser &operator=(FormulaParser const &obj)
@@ -51,6 +59,15 @@ class FormulaParser
 			{
 				_first_expression_terms = obj._first_expression_terms;
 				_second_expression_terms = obj._second_expression_terms;
+				_i = obj._i;
+				_spaceless_formula = obj._spaceless_formula;
+				_current_expression_terms = obj._current_expression_terms;
+				_current_coefficient = obj._current_coefficient;
+				_current_unknown_degree = obj._current_unknown_degree;
+				_is_constant = obj._is_constant;
+				_is_negative = obj._is_negative;
+				_is_exponant = obj._is_exponant;
+				_is_multiplication = obj._is_multiplication;
 			}
 			return (*this);
 		}
@@ -62,22 +79,11 @@ class FormulaParser
 		}
 
 	private:
-		void parse_formula(const std::string &formula)
+		void parse_formula(void)
 		{
-			std::vector<EquationTerm>	*current_expression_terms(&_first_expression_terms);
-			float current_coefficient(0.0f);
-			float temporary_exponant(0.0f);
-			int check_unknown_degree(0);
-			int current_unknown_degree(0);
-			bool is_constant(true);
-			bool is_negative(false);
-			bool is_exponant(false);
-			bool is_multiplication(false);
-			size_t convert_offset(0);
-
-			for (size_t i(0); i != formula.size() + 1; ++i)
+			for (; _i != _spaceless_formula.size() + 1; ++_i)
 			{
-				switch (formula[i])
+				switch (_spaceless_formula[_i])
 				{
 					case '0':
 					case '1':
@@ -89,101 +95,134 @@ class FormulaParser
 					case '7':
 					case '8':
 					case '9':
-						if (is_exponant == true)
-						{
-							temporary_exponant = std::stof(formula.substr(i), &convert_offset);
-							if (is_constant == true)
-								current_coefficient = std::pow(current_coefficient, temporary_exponant);
-							else if (is_constant == false)
-							{
-								if (is_multiplication == true)
-									current_unknown_degree += temporary_exponant;
-								else if (is_multiplication == false)
-									current_unknown_degree = temporary_exponant;
-								check_unknown_degree = temporary_exponant;
-								if (temporary_exponant != check_unknown_degree)
-									throw std::invalid_argument("Invalid exponant");
-								is_multiplication = false;
-							}
-							is_exponant = false;
-						}
-						else
-						{
-							if (is_multiplication == true)
-								current_coefficient *= std::stof(formula.substr(i), &convert_offset);
-							else
-							{
-								if (current_coefficient != 0.0f)
-									throw std::invalid_argument("Looks like the formula is not well formatted");
-								current_coefficient = std::stof(formula.substr(i), &convert_offset);
-							}
-							current_coefficient = is_negative ? -current_coefficient : current_coefficient;
-						}
-						i += convert_offset - 1;
+						_digit_case();
 						break;
 					case '-':
-						is_negative = true;
-						if (is_multiplication == true)
-							break;
-						if (i != 0)
-						{
-							current_expression_terms->push_back(EquationTerm(current_coefficient
-																		, current_unknown_degree));
-							current_coefficient = 0.0f;
-							current_unknown_degree = 0;
-							is_constant = true;
-							is_exponant = false;
-							is_multiplication = false;
-						}
+						_minus_case();
 						break;
 					case '\0':
 					case '+':
 					case '=':
-						current_expression_terms->push_back(EquationTerm(current_coefficient
-																		, current_unknown_degree));
-						current_coefficient = 0.0f;
-						current_unknown_degree = 0;
-						is_negative = false;
-						is_constant = true;
-						is_exponant = false;
-						is_multiplication = false;
-						if (formula[i] == '=')
-							current_expression_terms = &_second_expression_terms;
+						_plus_equal_null_case();
 						break;
 					case '*':
-						is_multiplication = true;
+						_is_multiplication = true;
 						break;
 					case 'X':
-						if (i == 0
-							|| formula[i - 1] == '+'
-							|| formula[i - 1] == '-'
-							|| formula[i - 1] == '=')
-							current_coefficient = is_negative ? -1.0f : 1.0f;
-						is_constant = false;
-						if (formula[i + 1] != '^')
-						{
-							if (is_multiplication == true)
-								++current_unknown_degree;
-							else
-								current_unknown_degree = 1;
-							is_multiplication = false;
-						}
+						_unknown_case();
 						break;
 					case '^':
-						is_exponant = true;
+						_is_exponant = true;
 						break;
 					default:
-						throw std::invalid_argument(std::string("Unexpected character found in the formula: ") + formula[i]);
+						throw std::invalid_argument(std::string("Unexpected character found in the formula: ") + _spaceless_formula[_i]);
 				}
 			}
-			if (isdigit(formula.back()) == false && formula.back() != 'X')
+			if (isdigit(_spaceless_formula.back()) == false && _spaceless_formula.back() != 'X')
 				throw std::invalid_argument("Formula looks incomplete");
-			if (current_expression_terms == &_first_expression_terms)
+			if (_current_expression_terms == &_first_expression_terms)
 				_second_expression_terms.push_back(EquationTerm(0.0f, 0));
+		}
+
+		void _digit_case(void)
+		{
+			float temporary_exponant(0.0f);
+			int check_unknown_degree(0);
+			size_t convert_offset(0);
+			
+			if (_is_exponant == true)
+			{
+				temporary_exponant = std::stof(_spaceless_formula.substr(_i), &convert_offset);
+				if (_is_constant == true)
+					_current_coefficient = std::pow(_current_coefficient, temporary_exponant);
+				else if (_is_constant == false)
+				{
+					if (_is_multiplication == true)
+						_current_unknown_degree += temporary_exponant;
+					else if (_is_multiplication == false)
+						_current_unknown_degree = temporary_exponant;
+					check_unknown_degree = temporary_exponant;
+					if (temporary_exponant != check_unknown_degree)
+						throw std::invalid_argument("Invalid exponant");
+					_is_multiplication = false;
+				}
+				_is_exponant = false;
+			}
+			else
+			{
+				if (_is_multiplication == true)
+					_current_coefficient *= std::stof(_spaceless_formula.substr(_i), &convert_offset);
+				else
+				{
+					if (_current_coefficient != 0.0f)
+						throw std::invalid_argument("Looks like the formula is not well formatted");
+					_current_coefficient = std::stof(_spaceless_formula.substr(_i), &convert_offset);
+				}
+				_current_coefficient = _is_negative ? -_current_coefficient : _current_coefficient;
+			}
+			_i += convert_offset - 1;			
+		}
+
+		void _minus_case(void)
+		{
+			_is_negative = true;
+			if (_is_multiplication == true)
+				return;
+			if (_i != 0)
+			{
+				_current_expression_terms->push_back(EquationTerm(_current_coefficient
+															, _current_unknown_degree));
+				_current_coefficient = 0.0f;
+				_current_unknown_degree = 0;
+				_is_constant = true;
+				_is_exponant = false;
+				_is_multiplication = false;
+			}
+		}
+
+		void _plus_equal_null_case(void)
+		{
+			_current_expression_terms->push_back(EquationTerm(_current_coefficient
+															, _current_unknown_degree));
+			_current_coefficient = 0.0f;
+			_current_unknown_degree = 0;
+			_is_negative = false;
+			_is_constant = true;
+			_is_exponant = false;
+			_is_multiplication = false;
+			if (_spaceless_formula[_i] == '=')
+				_current_expression_terms = &_second_expression_terms;
+		}
+
+		void _unknown_case(void)
+		{
+			if (_i == 0
+				|| _spaceless_formula[_i - 1] == '+'
+				|| _spaceless_formula[_i - 1] == '-'
+				|| _spaceless_formula[_i - 1] == '=')
+				_current_coefficient = _is_negative ? -1.0f : 1.0f;
+			_is_constant = false;
+			if (_spaceless_formula[_i + 1] != '^')
+			{
+				if (_is_multiplication == true)
+					++_current_unknown_degree;
+				else
+					_current_unknown_degree = 1;
+				_is_multiplication = false;
+			}
 		}
 
 		std::vector<EquationTerm>	_first_expression_terms;
 		std::vector<EquationTerm>	_second_expression_terms;
+		size_t						_i;
+		std::string					_spaceless_formula;
+		std::vector<EquationTerm>	*_current_expression_terms;
+		float 						_current_coefficient;
+		int 						_current_unknown_degree;
+		bool 						_is_constant;
+		bool 						_is_negative;
+		bool 						_is_exponant;
+		bool 						_is_multiplication;
 };
 
 #endif
