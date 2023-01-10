1a35a4796c6cecb4ef66851e459b59006dd90b94
Lucas DUTRIEZ <ldutriez@e1r13p3.clusters.42paris.fr>
2023-01-10 18:01:23 +0100

    ✅ Add test about complex solution in a + bi.

--- tests/unit_test.sh
+++ tests/unit_test.sh
@@ -42,6 +42,7 @@ fi
 ./"$FILE" "X^2 = -2" >> "$OUTPUT_FILE" 2>&1
 ./"$FILE" "X^2 = -3" >> "$OUTPUT_FILE" 2>&1
 ./"$FILE" "X^2 = -4" >> "$OUTPUT_FILE" 2>&1
+./"$FILE" "X^2+ X = -100" >> "$OUTPUT_FILE" 2>&1
 
 diff --expand-tabs --ignore-tab-expansion --side-by-side --left-column tests/output.log tests/expected_output.log > tests/diff.log
 echo "tests/diff.log created, please check it for differences between expected and actual output"
