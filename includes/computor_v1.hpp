96d2a69892ee4ac2dcbe326d66ab3b41b935314f
Lucas DUTRIEZ <ldutriez@e1r13p3.clusters.42paris.fr>
2023-01-10 22:26:20 +0100

    🎨 Remove of unnecessary prototype.

--- includes/computor_v1.hpp
+++ includes/computor_v1.hpp
@@ -6,7 +6,7 @@
 /*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/25 11:20:19 by ldutriez          #+#    #+#             */
-/*   Updated: 2023/01/10 02:46:48 by ldutriez         ###   ########.fr       */
+/*   Updated: 2023/01/10 22:22:21 by ldutriez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 # include <iostream>
 # include "EquationSolver.hpp"
 
-float squareRoot(float number, int precision);
 float power(float number, float power);
 
 #endif
